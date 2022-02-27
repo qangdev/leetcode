ad303ad170654f43d51cd9752c9891f7819e05a7
Qang D Tran <qangdev@gmail.com>
2022-02-27 19:01:01 +0700

    Rotate array (super simple)

--- lib/rotate_array.py
+++ lib/rotate_array.py
@@ -1,15 +1,7 @@
 
 
 def rotate_array(nums, k):
-    l, r = 0, k + 1
-    for i in range(0, k):
-        print(">", i)
-        li = nums[l]
-        ri = nums[r]
-        print(li, ri, "|", l, r)
-        nums[l] = ri
-        nums[r] = li
-        print(nums)
-        l += 1
-        r += 1
+    for _ in range(0, k):
+        x = nums.pop()
+        nums.insert(0, x)
     return nums
